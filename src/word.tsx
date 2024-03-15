fe2ff76d378d8667a9b1d76fba2f4111a53c34ae
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-03-14 22:07:13 -0700

    chore: delete an unnecessary file

--- src/word.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { View } from "react-native";
-
-export default function WordPage({ navigation }: any) {
-  return <View className="flex h-screen w-full flex-col px-4 py-6"></View>;
-}
